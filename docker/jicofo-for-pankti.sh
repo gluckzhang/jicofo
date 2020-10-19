7aa662f2b93507e7e57cd45fa16729177f123765
gluckzhang <zhanglong3030@qq.com>
2020-10-19 12:08:12 +0200

    attach jacoco to collect production execution data at the same time

--- docker/jicofo-for-pankti.sh
+++ docker/jicofo-for-pankti.sh
@@ -3,6 +3,8 @@
 JAVA_SYS_PROPS="-Dnet.java.sip.communicator.SC_HOME_DIR_LOCATION=/ -Dnet.java.sip.communicator.SC_HOME_DIR_NAME=config -Djava.util.logging.config.file=/config/logging.properties"
 
 JVM_OPTS="--add-opens java.base/jdk.internal.loader=ALL-UNNAMED --add-opens java.base/jdk.internal.module=ALL-UNNAMED --add-opens java.base/java.lang.module=ALL-UNNAMED --add-opens java.base/jdk.internal.util.jar=ALL-UNNAMED"
+GLOWROOT_OPTS="-javaagent:/home/jicofo/glowroot/glowroot.jar"
+JACOCO_OPTS="-javaagent:/home/jicofo/jacocoagent.jar=output=tcpserver,address=*,port=33333"
 
 JICOFO_OPTS="--domain=$XMPP_DOMAIN --host=$XMPP_SERVER --secret=$JICOFO_COMPONENT_SECRET --user_name=$JICOFO_AUTH_USER --user_domain=$XMPP_AUTH_DOMAIN --user_password=$JICOFO_AUTH_PASSWORD"
 
@@ -19,4 +21,4 @@ fi
 
 if [ -z "$JICOFO_MAX_MEMORY" ]; then JICOFO_MAX_MEMORY=3072m; fi
 
-JAVA_SYS_PROPS=$JAVA_SYS_PROPS java $JVM_OPTS -javaagent:/home/jicofo/glowroot/glowroot.jar -Xmx$JICOFO_MAX_MEMORY -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -Djdk.tls.ephemeralDHKeySize=2048 $LOGGING_CONFIG_PARAM $JAVA_SYS_PROPS -cp $cp $mainClass $JICOFO_OPTS
+JAVA_SYS_PROPS=$JAVA_SYS_PROPS java $JVM_OPTS $GLOWROOT_OPTS $JACOCO_OPTS -Xmx$JICOFO_MAX_MEMORY -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -Djdk.tls.ephemeralDHKeySize=2048 $LOGGING_CONFIG_PARAM $JAVA_SYS_PROPS -cp $cp $mainClass $JICOFO_OPTS
